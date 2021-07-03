8a4653592c4bd7111c00f80d3b756ecfc6039ce5
soumyadip-halder <soumyadip.halder@gmail.com>
2021-07-03 13:20:42 +0530

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header(props) {
+  return (
+    <div className="card">
+      <div className="btn btn-primary font-weight-bold">Header</div>
+    </div>
+  );
+}
+
+export default Header;
